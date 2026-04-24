eb85d385416f36e58e1af3c9264a51136d145d21
Daneelka <danah29122003@gmail.com>
2026-04-24 14:00:48 +0300

    fix cpplint

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,8 +2,7 @@
 #include <cstdint>
 #include "alg.h"
 
-bool checkPrime(uint64_t value)
-{
+bool checkPrime(uint64_t value) {
   if (value < 2)
     return false;
 
@@ -13,8 +12,7 @@ bool checkPrime(uint64_t value)
   if (value % 2 == 0)
     return false;
 
-  for (uint64_t i = 3; i * i <= value; i += 2)
-  {
+  for (uint64_t i = 3; i * i <= value; i += 2) {
     if (value % i == 0)
       return false;
   }
@@ -22,19 +20,16 @@ bool checkPrime(uint64_t value)
   return true;
 }
 
-uint64_t nPrime(uint64_t n)
-{
+uint64_t nPrime(uint64_t n) {
   if (n == 0)
     return 0;
 
   uint64_t count = 0;
   uint64_t number = 1;
 
-  while (count < n)
-  {
+  while (count < n) {
     number++;
-    if (checkPrime(number))
-    {
+    if (checkPrime(number)) {
       count++;
     }
   }
@@ -42,29 +37,24 @@ uint64_t nPrime(uint64_t n)
   return number;
 }
 
-uint64_t nextPrime(uint64_t value)
-{
+uint64_t nextPrime(uint64_t value) {
   uint64_t number = value + 1;
 
-  while (true)
-  {
+  while (true) {
     if (checkPrime(number))
       return number;
     number++;
   }
 }
 
-uint64_t sumPrime(uint64_t hbound)
-{
+uint64_t sumPrime(uint64_t hbound) {
   if (hbound <= 2)
     return 0;
 
   uint64_t sum = 2;
 
-  for (uint64_t i = 3; i < hbound; i += 2)
-  {
-    if (checkPrime(i))
-    {
+  for (uint64_t i = 3; i < hbound; i += 2) {
+    if (checkPrime(i)) {
       sum += i;
     }
   }
--- test/tests.cpp
+++ test/tests.cpp
@@ -4,56 +4,46 @@
 #include <cstdint>
 #include "alg.h"
 
-TEST(checkPrime, BasicPrimes)
-{
+TEST(checkPrime, BasicPrimes) {
   EXPECT_TRUE(checkPrime(2));
   EXPECT_TRUE(checkPrime(3));
   EXPECT_TRUE(checkPrime(5));
 }
 
-TEST(checkPrime, BasicNotPrimes)
-{
+TEST(checkPrime, BasicNotPrimes) {
   EXPECT_FALSE(checkPrime(1));
   EXPECT_FALSE(checkPrime(4));
   EXPECT_FALSE(checkPrime(9));
 }
 
-TEST(nPrime, FirstPrime)
-{
+TEST(nPrime, FirstPrime) {
   EXPECT_EQ(2u, nPrime(1));
 }
 
-TEST(nPrime, ThirdPrime)
-{
+TEST(nPrime, ThirdPrime) {
   EXPECT_EQ(5u, nPrime(3));
 }
 
-TEST(nPrime, FifthPrime)
-{
+TEST(nPrime, FifthPrime) {
   EXPECT_EQ(11u, nPrime(5));
 }
 
-TEST(nextPrime, AfterComposite)
-{
+TEST(nextPrime, AfterComposite) {
   EXPECT_EQ(5u, nextPrime(4));
 }
 
-TEST(nextPrime, AfterPrime)
-{
+TEST(nextPrime, AfterPrime) {
   EXPECT_EQ(13u, nextPrime(11));
 }
 
-TEST(sumPrime, LessThanTwo)
-{
+TEST(sumPrime, LessThanTwo) {
   EXPECT_EQ(0u, sumPrime(2));
 }
 
-TEST(sumPrime, UpToTen)
-{
+TEST(sumPrime, UpToTen) {
   EXPECT_EQ(17u, sumPrime(10));
 }
 
-TEST(sumPrime, UpToTwenty)
-{
+TEST(sumPrime, UpToTwenty) {
   EXPECT_EQ(77u, sumPrime(20));
 }
